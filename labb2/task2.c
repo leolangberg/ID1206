d8ab27d07da94ed1877cb8908689b76775048b51
Leo Långberg <langbergleo@gmail.com>
2024-11-15 14:51:34 +0100

    task2.

--- labb2/task2.c
+++ labb2/task2.c
@@ -9,42 +9,55 @@
 // like atomic_flag_test_and_set() to make the following program run.
 
 int counter = 0;
-const int nthr = 1000;
+const int nthr = 1000; // for some reason threads cap at 2^11 = 2048;
 
 typedef struct {
         /* members to be defined */
+        atomic_flag lock;
 } mysemaphore_t;
 
 mysemaphore_t sem;
 
 int sem_init(mysemaphore_t *s) {
-        /* function to be implemented */
+        /* Initialize flag by clearing it. */
+        atomic_flag_clear(&s->lock);
         return 0;
 }
 
 int sem_wait(mysemaphore_t *s) {
-        /* function to be implemented */
+        /* Continously check if lock has become available. */
+        while(atomic_flag_test_and_set(&s->lock)) {
+                // busy wait.
+        }
         return 0;
 }
 
 int sem_post(mysemaphore_t *s) {
-        /* function to be implemented */
+        /* Unlock semaphore. */
+        atomic_flag_clear(&s->lock);
         return 0;
 }
 
 void *func() {
         sleep(1);
         sem_wait(&sem);
-        counter++;
+        //printf("Thread %ld entering critical section\n", (long) pthread_self());
+        counter++;         // Critical section
+        //printf("Thread %ld leaving  critical section\n", (long) pthread_self());
         sem_post(&sem);
+    
 }
 
 int main() {
         pthread_t thr[nthr];
+        pthread_attr_t attr;
+        size_t stack_size = nthr;
+        pthread_attr_init(&attr);
+        pthread_attr_setstacksize(&attr, stack_size);
         sem_init(&sem);
 
         for(int i = 0; i < nthr; i++) {
-                pthread_create(&thr[i], NULL, &func, NULL);
+                pthread_create(&thr[i], &attr, &func, NULL);
         }
 
         for(int i = 0; i < nthr; i++) {
