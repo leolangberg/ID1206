50de402e55ac9bb255e3980028e9f6b7e56eb5d7
Leo Långberg <langbergleo@gmail.com>
2024-11-15 15:07:19 +0100

    task3.

--- labb2/task3.c
+++ labb2/task3.c
@@ -5,8 +5,24 @@
 #include <pthread.h>
 #include <stdatomic.h>
 
-// Implement your own binary semaphore lock using atomic functions
-// like atomic_flag_test_and_set() to make the following program run.
+// To mitigate the impact of busy waiting. Tom Anderson suggested to add
+// a delay after each attempt to acquire a lock. This delay may be
+// incremented in each iteration as shown in the following pseudo-code.
+// --------------------------------------------------------------------------
+//  type lock = (unlocked, locked)
+//
+//  procedure acquire_lock (L : ^lock)
+//      delay : integer := 1
+//      while test_and_set (L) = locked     // returns old value
+//          pause(delay)                    // consume this many units of time
+//          delay := delay * 2
+//
+//  procedure release_lock (L : ^lock)
+//      lock^ := unlocked
+// --------------------------------------------------------------------------
+// Update the code implemented in the previous task by adding a delay using
+// the POSIX function usleep(). Check whether performance differences
+// can be observed.
 
 int counter = 0;
 const int nthr = 1000; // for some reason threads cap at 2^11 = 2048;
@@ -26,8 +42,11 @@ int sem_init(mysemaphore_t *s) {
 
 int sem_wait(mysemaphore_t *s) {
         /* Continously check if lock has become available. */
+        int delay = 1;
         while(atomic_flag_test_and_set(&s->lock)) {
-                // busy wait.
+                // Mitigate busy waiting.
+                usleep(delay);
+                delay = delay * 2;
         }
         return 0;
 }
@@ -41,9 +60,7 @@ int sem_post(mysemaphore_t *s) {
 void *func() {
         sleep(1);
         sem_wait(&sem);
-        //printf("Thread %ld entering critical section\n", (long) pthread_self());
         counter++;         // Critical section
-        //printf("Thread %ld leaving  critical section\n", (long) pthread_self());
         sem_post(&sem);
     
 }
@@ -62,4 +79,6 @@ int main() {
 
         printf("counter = %d\n", counter);
         return 0;
+
+        // Kolla performance difference med task2.
 }
